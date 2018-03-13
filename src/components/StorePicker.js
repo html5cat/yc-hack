7ad7bb25643727a962975be2730a16fb50a08bb8
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-12 23:35:15 -0700

    Add store change handler

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,21 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 export default class StorePicker extends React.Component {
+  storeName = React.createRef()
+
+  goToStore = (event) => {
+    event.preventDefault()
+    const name = this.storeName.value.value
+    this.props.history.push(`/store/${name}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
       <h2>Please enter a store</h2>
         <input
           type="text"
+          ref={this.storeName}
           required
           placeholder={"Store Name"}
           defaultValue={getFunName()}
