00c9731eb12ec33fc411a2fed655b793b4d15dfe
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-13 12:06:38 -0700

    Add AddFish form saves to state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,17 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 export default class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+
+  addFish = (fish) => {
+    const fishes = {...this.state.fishes}
+    fishes[`fish-${Date.now()}`] = fish
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +22,7 @@ export default class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
