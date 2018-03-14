94c1b1d1585a612f8d8f21540c432f7bbe15f00d
Yuriy Dybskiy <yuriy@dybskiy.com>
2018-03-13 23:40:21 -0700

    Add Firebase authentication and store ownership

--- src/components/App.js
+++ src/components/App.js
@@ -85,6 +85,7 @@ export default class App extends React.Component {
           deleteFish={this.deleteFish}
           fishes={this.state.fishes}
           loadSampleFishes={this.loadSampleFishes}
+          storeId={this.props.match.params.storeId}
         />
       </div>
     )
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import firebase from 'firebase'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
+import Login from './Login'
+import base, { firebaseApp } from '../base';
 
 export default class Inventory extends React.Component {
   static propTypes = {
@@ -10,10 +13,67 @@ export default class Inventory extends React.Component {
     deleteFish: PropTypes.func
   }
 
+  state = {
+    uid: null,
+    owner: null
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.authHandler({ user })
+      }
+    })
+  }
+
+  authHandler = async (authData) => {
+    const store = await base.fetch(this.props.storeId, {
+      context: this
+    })
+
+    if (!store.owner) {
+      await base.post(`${this.props.storeId}/owner`, {
+        data: authData.user.uid
+      })
+    }
+
+    this.setState({
+      uid: authData.user.uid,
+      owner: store.owner || authData.user.uid
+    })
+
+  }
+
+  authenticate = (provider) => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]()
+    firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler)
+  }
+
+  logout = async () => {
+    await firebase.auth().signOut()
+    this.setState({ uid: null })
+  }
+
   render() {
+    const logout = <button onClick={this.logout}>Logout</button>
+
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />
+    }
+
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Only the owner of this store can edit the Inventory, sorry.</p>
+          {logout}
+        </div>
+      )
+    }
+
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map((key) => (
           <EditFishForm
             key={key}
